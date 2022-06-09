c6772ca17a4912dbb80d0a227417b250ea37f5b7
Seabiscuit <mzy3991@gmail.com>
2022-06-09 16:42:37 +0800

    feature: edit-at-point

--- init.el
+++ init.el
@@ -23,10 +23,6 @@
 
 
 
-
-
-(setq inhibit-startup-message t)
-
 (require 'package)
 (setq package-enable-at-startup nil)
 (add-to-list 'package-archives
@@ -42,12 +38,14 @@
 ;; (setq indo-enable-flex-matching t)
 ;; (setq ido-everywhere t)
 ;; (ido-mode 1)
-
 ;;///////////////////////////////////////////////////////////
 ;;///////////////    SETTINGS      //////////////////////////
 ;;///////////////////////////////////////////////////////////
+(custom-set-faces
+ '(default ((t (:height 150 :family "Hermit"))))
+ )
 
-
+(setq inhibit-startup-message t)
 
 (setq custom-tab-width 2)
 
@@ -167,7 +165,7 @@
       "cp" 'evilnc-comment-or-uncomment-paragraphs
       "=" 'er-indent-region-or-buffer
       "qq" 'save-buffers-kill-terminal
-      "qR" 'restart-emacs
+      "qr" 'restart-emacs
 
       ;;```````````````````````````` Tools
       "op" 'youdao-dictionary-search-at-point-tooltip
@@ -187,25 +185,32 @@
 ;;/////////////////////    CUSTOM FUNC   ////////////////////
 ;;^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 ;; ---------------- copy word ---------------
-(defun get-point (symbol &optional arg)
-  "get the point"
-  (funcall symbol arg)
-  (point))
+(use-package edit-at-point
+  :ensure t)
 
-(defun copy-thing (begin-of-thing end-of-thing &optional arg)
-  "Copy thing between beg & end into kill ring."
-  (save-excursion
-    (let ((beg (get-point begin-of-thing 1))
-          (end (get-point end-of-thing arg)))
-      (copy-region-as-kill beg end))))
-
-(defun copy-word (&optional arg)
-  "Copy words at point into kill-ring"
-  (interactive "P")
-  (copy-thing 'backward-word 'forward-word arg)
-  ;;(paste-to-mark arg)
+(defun mzy/edit-at-point-cut-word ()
+  (interactive)
+  (edit-at-point-word-cut)
+  (evil-insert 1)
   )
-(define-key evil-normal-state-map (kbd ", a a") 'copy-word)
+(defun mzy/evil-ex-s ()
+  (interactive)
+  (evil-ex "'<,'>s/")
+  )
+
+(defun mzy/evil-ex-%s ()
+  (interactive)
+  (evil-ex "%s/")
+  )
+
+(define-key evil-normal-state-map (kbd "; a a") 'edit-at-point-word-copy)
+(define-key evil-normal-state-map (kbd "; c c") 'mzy/edit-at-point-cut-word)
+(define-key evil-normal-state-map (kbd "; s c") 'edit-at-point-word-cut)
+(define-key evil-normal-state-map (kbd "; s r") 'mzy/evil-ex-s)
+(define-key evil-normal-state-map (kbd "; r r") 'mzy/evil-ex-%s)
+(define-key evil-normal-state-map (kbd "; s p") 'edit-at-point-word-paste)
+(define-key evil-normal-state-map (kbd "; s d") 'edit-at-point-word-delete)
+
 (define-key evil-visual-state-map (kbd "v") 'er/expand-region)
 (define-key evil-visual-state-map (kbd "V") 'er/contract-region)
 
@@ -683,3 +688,7 @@ The app is chosen from your OS's preference."
   (setq evil-escape-excluded-major-modes '(dired-mode))
   (setq-default evil-escape-key-sequence "kj")
   (evil-escape-mode 1))
+
+(use-package window-purpose
+  :ensure t
+  :config)
